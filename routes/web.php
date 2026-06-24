ac47590033ffa792d2a287d2fee6583a571091f6
ANAND KUMAR JHA <skill.jha@gmail.com>
2026-06-24 15:19:30 +0530

    api added

--- routes/web.php
+++ routes/web.php
@@ -6,5 +6,5 @@ use Illuminate\Foundation\Inspiring;
 Route::get('/', function () {
     $qoute = Inspiring::quote();
 
-    return view('welcome', ["qoute"=>$qoute, "name"=>"Anand Jha"]);
+    return view('welcome', ["qoute"=>$qoute]);
 });
